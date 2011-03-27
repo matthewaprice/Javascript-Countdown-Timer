3b5df3160885989802b109b489e554e62cb0cac9
Matthew Price <matthew@matthewaprice.com>
2011-03-27 15:24:55 -0600

    first timer.js commit

---
+++ timer.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	
+	function makeTimers() {
+		
+		$('div.timer span').each(function() {
+			
+			var endTime = $(this).attr('id');			
+			var endTime = endTime.replace('_','');
+			
+			var now = new Date();
+			var now = (Date.parse(now) / 1000);
+			
+			var timeLeft = endTime - now;
+			
+			var days = Math.floor(timeLeft / 86400); 
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+			
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+										
+			$(this).html(days + ":" + hours + ":" + minutes + ":" + seconds);		
+			
+		});
+		
+	}
+	
+	setInterval(function() { makeTimers(); }, 1000);
+
+});	
