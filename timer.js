d92f2a5725c5ac07781b8008d1bc6f905732d98b
Matthew Price <matthew@matthewaprice.com>
2011-10-06 13:09:00 -0600

    First commit of non-jquery script

--- timer.js
+++ timer.js
@@ -1,34 +1,29 @@
-$(document).ready(function() {
-
-	function makeTimer() {
-			
-			var endTime = new Date("September 01, 2011 00:00:00");			
-			var endTime = (Date.parse(endTime)) / 1000;
-			
-			var now = new Date();
-			var now = (Date.parse(now) / 1000);
-			
-			var timeLeft = endTime - now;
-			
-			var days = Math.floor(timeLeft / 86400); 
-			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-			
-			if (hours < "10") { hours = "0" + hours; }
-			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
-										
-			$("#timer #days").html("<span>Days</span>" + days);
-			$("#timer #hours").html("<span>Hours</span>" + hours);
-			$("#timer #minutes").html("<span>Minutes</span>" + minutes);
-			$("#timer #seconds").html("<span>Seconds</span>" + seconds);		
-		
-	}
+function CFProjectTimer( time_remaining ) {
 	
-	setInterval(function() { makeTimer(); }, 1000);
+	// End time is in this format January 01, 2012 09:00:00
+	var endTime = new Date( time_remaining );			
+	var endTime = (Date.parse(endTime)) / 1000;
 	
-});
+	var now = new Date();
+	var now = (Date.parse(now) / 1000);
+	
+	var timeLeft = endTime - now;
+	
+	var days = Math.floor(timeLeft / 86400); 
+	var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+	var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+	var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+	
+	if (hours < "10") { hours = "0" + hours; }
+	if (minutes < "10") { minutes = "0" + minutes; }
+	if (seconds < "10") { seconds = "0" + seconds; }
+	
+	document.getElementById('project-remaining-days').innerHTML = '<span>Days</span>' + days;
+	document.getElementById('project-remaining-hours').innerHTML = '<span>Hours</span>' + hours;
+	document.getElementById('project-remaining-minutes').innerHTML = '<span>Minutes</span>' + minutes;
+	document.getElementById('project-remaining-seconds').innerHTML = '<span>Seconds</span>' + seconds;
+	
+}
 
 // HTML output with this method
 // <div id="timer">
